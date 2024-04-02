b7367d63b1eb77b9a82312950736f05914bc2ede
inde153 <inde153@naver.com>
2024-04-02 17:27:09 +0900

    Add : 5번째 문제 및 답

---
+++ solutions/questions5/solution5.js
@@ -0,0 +1,16 @@
+function solution(babbling) {
+  const vowels = ['aya', 'ye', 'woo', 'ma'];
+
+  return babbling.reduce((acc, ele) => {
+    for (const vowel of vowels) {
+      if (ele.includes(vowel.repeat(2))) {
+        return acc;
+      }
+      if (ele.includes(vowel)) {
+        ele = ele.replaceAll(vowel, ' ');
+      }
+    }
+    console.log(ele);
+    return ele.replaceAll(' ', '').length ? acc : acc + 1;
+  }, 0);
+}
